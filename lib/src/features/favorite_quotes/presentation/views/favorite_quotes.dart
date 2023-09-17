fecd255d7472ae60c5e03524ccf058c0a958bf5b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-09-17 10:12:40 +0300

    Create New Feature: FavoriteQuotes

---
+++ lib/src/features/favorite_quotes/presentation/views/favorite_quotes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavoriteQuotesView extends StatelessWidget {
+  const FavoriteQuotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Favorites"),
+      ),
+    );
+  }
+}
