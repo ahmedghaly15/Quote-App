c970b3a659dff36d49c0c697a1b4e0fa83314b97
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-09-20 14:13:38 +0300

    Handle AppInterceptors

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:quotes_app/src/core/api/app_interceptors.dart';
 import 'package:quotes_app/src/core/network/network_info.dart';
 import 'package:quotes_app/src/core/network/network_info_impl.dart';
 import 'package:quotes_app/src/features/random_quote/data/datasources/fav_datasource.dart';
@@ -88,4 +90,18 @@ Future<void> setUpServiceLocator() async {
   );
 
   serviceLocator.registerLazySingleton<List<QuoteEntity>>(() => []);
+  serviceLocator.registerLazySingleton<AppInterceptors>(
+    () => AppInterceptors(),
+  );
+
+  serviceLocator.registerLazySingleton<LogInterceptor>(
+    () => LogInterceptor(
+      request: true,
+      requestBody: true,
+      requestHeader: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+    ),
+  );
 }
